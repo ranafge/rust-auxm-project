3766819d7a1157e277d3a7d4063666f035462416
ranafge <ranafge@gmail.com>
2024-02-01 10:36:02 +0600

    added one route routes_hello

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+use std::net::SocketAddr;
+
+use axum::{response::Html, routing::get, Router};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+
+async fn main() {
+    let routes_hello = Router::new().route(
+        "/hello",
+        get(|| async {Html("Hello <strong>World!</strong>")})
+    );
+
+    // region: --start Server
+    // let addr = SocketAddr::from(([127,0,0,1], 8080));
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+
+    println!("->> LISTENING on {:?}\n", listener.local_addr());
+    axum::serve(listener, routes_hello)
+    .await
+    .unwrap();
+        // endregion --start Server
+    println!("End server");
 }
+
--- test_programs/src/deref_trait.rs
+++ test_programs/src/deref_trait.rs
@@ -4,6 +4,7 @@ pub fn deref_trait_understanding() {
         println!("I can read the book: {}", book)
     }
     let card2:Option<String> = Some("RUST BOOK ".to_string());
+    // as_deref trait convert option<String> to option<&str>
     if let Some(book) = card2.as_deref()  {
         println!("I CAN READ THIS BOOK {}", book);
     }
