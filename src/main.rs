2a71f6bc0dd3fe81d46c50da696db57c5128db7c
ranafge <ranafge@gmail.com>
2024-02-01 12:11:06 +0600

    test need to resovle

--- src/main.rs
+++ src/main.rs
@@ -17,10 +17,8 @@ async fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
 
     println!("->> LISTENING on {:?}\n", listener.local_addr());
-    axum::serve(listener, routes_hello)
-    .await
-    .unwrap();
+    axum::serve(listener, routes_hello.into_make_service());
         // endregion --start Server
-    println!("End server");
+    println!("Server running.");
 }
 
